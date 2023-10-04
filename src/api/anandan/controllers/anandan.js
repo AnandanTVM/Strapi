200d7dd21bfa73b5220e36d76af089e0303fcd6c
Anandan <anandan2016@gmail.com>
2023-10-04 12:25:31 +0530

    updated code

--- src/api/anandan/controllers/anandan.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * anandan controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::anandan.anandan');
--- src/api/anandan/routes/anandan.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * anandan router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::anandan.anandan');
